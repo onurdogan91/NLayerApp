4e2ff2baff76d0278e501676070df766881548c6
onurdogan91 <onurdogan@outlook.com.tr>
2022-12-21 19:32:14 +0300

    Data

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+        //new anahtar sözcüğü ile oluşturmasın diye abstract class kullanıyoruz...
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
