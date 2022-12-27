39e580b3023b0b73266f59c89b70bce40496c361
onurdogan91 <onurdogan@outlook.com.tr>
2022-12-27 17:34:23 +0300

    IProductService Updated

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -12,20 +12,18 @@ namespace NLayer.API.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
-        private readonly IProductService productService;
+        private readonly IProductService _service;
 
         public ProductsController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
-            _service = service;
-            this.productService = productService;
+            _service = productService;
         }
 
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
-            return CreateActionResult(await productService.GetProductWithCategory());
+            return CreateActionResult(await _service.GetProductWithCategory());
         }
 
         [HttpGet]
