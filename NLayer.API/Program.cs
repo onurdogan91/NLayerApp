3c69814910e906b384108654a2d4cecb53dd514c
onurdogan91 <onurdogan@outlook.com.tr>
2023-01-17 17:07:53 +0300

    ThrowFaultOnServiceLayer

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -66,7 +66,7 @@ namespace NLayer.API
 
             app.UseHttpsRedirection();
 
-            app.UserCustomException();
+            app.UseCustomException();
 
             app.UseAuthorization();
 
