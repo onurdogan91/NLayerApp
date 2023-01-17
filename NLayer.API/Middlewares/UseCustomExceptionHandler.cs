477eb0aa5c9d35ad9083dde31e46f3134141c6b0
onurdogan91 <onurdogan@outlook.com.tr>
2023-01-17 14:55:32 +0300

    Try

---
+++ NLayer.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace NLayer.API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UserCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run;
+            });
+        }
+    }
+}
